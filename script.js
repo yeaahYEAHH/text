89bc9119a1007264eba29be2e2efefa4f9ccc3a5
Yeah <81806907+yeaahYEAHH@users.noreply.github.com>
2022-10-02 13:07:53 +0500

    update

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 let addMeassage = document.querySelector('.mes'),
-    addButton = document.querySelector('.add');
+    addButton = document.querySelector('.add'),
+    addClean = document.querySelector('.clean');
+
+let ans = localStorage.getItem('value');
+document.getElementById('text').innerHTML = ans;
 
 addButton.addEventListener('click', function(){
-    document.getElementById('text').innerHTML = addMeassage.value;
-
+    localStorage.setItem('value',  addMeassage.value)
+    display()
 });
 
+addClean.addEventListener('click', function(){
+    document.getElementById('text').innerHTML = '';
+    localStorage.removeItem('value');
+})
+
+function display(){
+    document.getElementById('text').innerHTML = addMeassage.value;
+}
